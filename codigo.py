f9cb0702bbe25a25d042b5547b051d5d6d7f601d
Matias Bernal <matias.bernal.quintero@hotmail.com>
2024-12-20 15:18:11 -0500

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,1 +1,7 @@
-
+import random
+caracteres="+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+longitud=int(input("ingresa tu longitud: "))
+password=""
+for i in range(longitud):
+    password+=random.choice(caracteres)
+print(f"esta es tu contraseña: {password}")
